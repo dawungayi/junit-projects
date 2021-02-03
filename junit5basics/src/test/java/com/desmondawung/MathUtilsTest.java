044cae88994f6c079fccf9224adf9bf4950e8e8c
Desmond Awungayi <dawungayi@gmail.com>
2021-02-03 06:12:04 -0600

    Demonstrates various useful JUnit annotations - For managing a large number of tests @DisplayName and @Disabled - Conditional execution annotations: @EnabledOnOs, etc. Test is skipped if the condition is false. - Assumption methods: test is skipped if the assumption (made within the test method logic) is false.

--- junit5basics/src/test/java/com/desmondawung/MathUtilsTest.java
+++ junit5basics/src/test/java/com/desmondawung/MathUtilsTest.java
@@ -1,8 +1,13 @@
 package com.desmondawung;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class MathUtilsTest {
 
@@ -28,6 +33,8 @@ class MathUtilsTest {
 
 
     @Test
+    @EnabledOnOs(OS.LINUX)
+    @DisplayName("Testing add method")  // gives a clear indicator of what the test method is doing
     void testAdd() {
         // fail("Not yet implemented");
         // System.out.println("This test ran");
@@ -40,10 +47,16 @@ class MathUtilsTest {
 
 
     @Test
+    @DisplayName("Testing the divide method.")
     void testDivide() {
         // this test will fail for 2 reasons:
         // 1. exception was not thrown at all   2. the wrong exception was thrown
         // (expected type, executable (lambda))
+        boolean isServerUp = false;
+        // this test does not run if the "isServerUp" is not true. For example if a test needs a server up and running to work,
+        // assume the server is up before proceeding. if not up, don't run this test (and eventually break it)
+        // this helps us distinguish between tests failing cuz of my logic and tests failing cuz of external factors
+        assumeTrue(isServerUp);  // very different from assertion
         assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0),
                 "Divide by zero should throw.");
         // assertThrows(NullPointerException.class, () -> mathUtils.divide(1, 0),
@@ -54,12 +67,20 @@ class MathUtilsTest {
 
     // Test-driven methodology with JUnit
     @Test
+    @DisplayName("Testing the method that computes the area of the circle.")
     void testComputeCircleRadius() {
         double r = 10;
         double expected = 314.1592653589793;    // hard-copied from what it should be
         // double expected = Math.PI * r * r;
         double actual = mathUtils.computeCircleArea(r);
         assertEquals(expected, actual, "Should compute the correct area of a circle given its radius.");
+    }
+
+    @Test
+    @DisplayName("TDD method. Disable for now")
+    @Disabled
+    void disabledTest() {
+        fail("This test should be disabled.");
     }
 
 
