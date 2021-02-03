cbad19c457e446e3af1d3134c5865597d7ea6267
Desmond Awungayi <dawungayi@gmail.com>
2021-02-03 05:28:59 -0600

    Demonstrates JUnit lifecycle hooks annotations - Demonstrates how @BeforeEach annotation could be used to initialize a class once in code so a fresh class instance is created before each test. - Also demonstrates how @BeforeAll and @AfterAll need to be static for proper functioning.

--- junit5basics/src/test/java/com/desmondawung/MathUtilsTest.java
+++ junit5basics/src/test/java/com/desmondawung/MathUtilsTest.java
@@ -1,16 +1,36 @@
 package com.desmondawung;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
 
+    private MathUtils mathUtils;    // member variable
+
+    @BeforeEach     // runs before every test method is run
+    void init() {
+        mathUtils = new MathUtils();    // fresh instance every
+    }
+
+
+    // @BeforeAll and @AfterAll need to be static so they do not depend on creating a class instance for them to be run.
+    // remember that @BeforeAll is run before the class itself is init
+    @BeforeAll
+    static void beforeAllInit() {
+        System.out.println("Runs before class is init");
+    }
+
+    @AfterEach
+    void cleanup() {
+        System.out.println("Cleaning up after each method");
+    }
+
+
     @Test
     void testAdd() {
         // fail("Not yet implemented");
         // System.out.println("This test ran");
-        MathUtils mathUtils = new MathUtils();
         int expected = 5;
         int a = 2, b = 3;
         int actual = mathUtils.add(a,b);
@@ -18,9 +38,9 @@ class MathUtilsTest {
         assertEquals(expected, actual, "The add method should sum up two numbers.");
     }
 
+
     @Test
     void testDivide() {
-        MathUtils mathUtils = new MathUtils();
         // this test will fail for 2 reasons:
         // 1. exception was not thrown at all   2. the wrong exception was thrown
         // (expected type, executable (lambda))
@@ -31,10 +51,10 @@ class MathUtilsTest {
 
     }
 
+
     // Test-driven methodology with JUnit
     @Test
     void testComputeCircleRadius() {
-        MathUtils mathUtils = new MathUtils();
         double r = 10;
         double expected = 314.1592653589793;    // hard-copied from what it should be
         // double expected = Math.PI * r * r;
